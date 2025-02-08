d7e7274d8524c98ad3a1395bd3ac010f70d861dd
gigalataria <latariagiga008@gmail.com>
2025-02-08 15:05:51 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const user1 = "Giorgi"
+const age1 = 20
+
+const user2 = "Mariami"
+const age2 = 18
+
+const color1 = "blue"
+const color2= "green"
+const color3 = "red"
+
+const num1 = 5
+const num2 = -5
+const num3 = 15 * 3
+const num4 = 15 + 3
+const num5 = 15 - 3
+const num6 = 15 / 3
+
+const num7= 123 * 654
+
+console.log(num7)
+console.log(num3)
+console.log(num2)
+console.log(num4)
+console.log(num5)
+console.log(num6)
